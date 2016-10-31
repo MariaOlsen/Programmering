97da7dca1536f8a940d7e2891d1b3fbecba4e3df
maria <maria@10.200.120.95>
2016-10-31 11:30:40 +0100

    Tilføjet klassen "Klasse"

---
+++ src/githubtest/Klasse.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author maria
+ */
+public class Klasse {
+    
+    public Klasse(){
+        System.out.println("Halløj");
+    }
+    
+}
